21113c6de60dc757f84e7dc98def6c6409ee3242
Liel <liel.izcha321@gmail.com>
2021-11-05 12:56:19 +0200

    add names

--- javascript/nain.js
+++ javascript/nain.js
@@ -2,7 +2,6 @@ let homeTitle = "Hello, I'm Liel Itzchak and I'm a Full-stack Developer...";
 let homePageMain = document.getElementById("homePageMain");
 let TitleHomePage = document.getElementById("TitleHomePage");
 let projectsArticle = document.getElementById("projectsArticle");
-// let projectsArticle = document.getElementById("projectsArticle");
 let projectSection = document.getElementById("projectsArticle");
 let i = 0;
 let id = setInterval(() => {
@@ -12,24 +11,71 @@ let id = setInterval(() => {
   }
 }, 99);
 
+class Project {
+  name;
+  description;
+  link;
+  photo;
+  id;
+  constructor(name, description, link, photo) {
+    this.name = name;
+    this.description = description;
+    this.link = link;
+    this.photo = photo;
+    this.id = ++Project.id;
+  }
+  static id = 0;
+}
+
 const MY_PROJECTS = [
   new Project(
     "hotel project",
-    "",
+    '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, repellat autem minima voluptatem reiciendis ipsam id quisquam quod! Laborum libero pariatur impedit maxime molestias consectetur neque voluptatum temporibus, minima"',
     "https://did.li/hZNw5",
     "https://did.li/1sINf"
   ),
-  new Project("spotify", "", "https://did.li/Fvarl", "https://did.li/1sINf"),
-  new Project("DJ Chekol", "", "", "https://did.li/1sINf"),
-  new Project("landing page", "", "", "https://did.li/1sINf"),
+  new Project(
+    "spotify",
+    '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, repellat autem minima voluptatem reiciendis ipsam id quisquam quod! Laborum libero pariatur impedit maxime molestias consectetur neque voluptatum temporibus, minima"',
+    "https://did.li/Fvarl",
+    "https://did.li/1sINf"
+  ),
+  new Project(
+    "DJ Chekol",
+    '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, repellat autem minima voluptatem reiciendis ipsam id quisquam quod! Laborum libero pariatur impedit maxime molestias consectetur neque voluptatum temporibus, minima"',
+    "",
+    "https://did.li/1sINf"
+  ),
+  new Project(
+    "landing page",
+    '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, repellat autem minima voluptatem reiciendis ipsam id quisquam quod! Laborum libero pariatur impedit maxime molestias consectetur neque voluptatum temporibus, minima"',
+    "",
+    "https://did.li/1sINf"
+  ),
   new Project(
     "Clothing-Store-project",
-    "",
+    '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, repellat autem minima voluptatem reiciendis ipsam id quisquam quod! Laborum libero pariatur impedit maxime molestias consectetur neque voluptatum temporibus, minima"',
     "https://did.li/trrCN",
     "https://did.li/1sINf"
   ),
-  new Project("join project", "", "", "https://did.li/1sINf"),
-  new Project("calculator", "", "https://did.li/KCTfT", "https://did.li/1sINf"),
+  new Project(
+    "join project",
+    '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, repellat autem minima voluptatem reiciendis ipsam id quisquam quod! Laborum libero pariatur impedit maxime molestias consectetur neque voluptatum temporibus, minima"',
+    "",
+    "https://did.li/1sINf"
+  ),
+  new Project(
+    "calculator",
+    '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, repellat autem minima voluptatem reiciendis ipsam id quisquam quod! Laborum libero pariatur impedit maxime molestias consectetur neque voluptatum temporibus, minima"',
+    "https://did.li/KCTfT",
+    "../media/photos/calculator.jpg"
+  ),
+  new Project(
+    "movie",
+    "my first project with API ' that i didn't build",
+    "https://did.li/Qkgaa",
+    ""
+  ),
 ];
 
 function printToDomAllProject() {
