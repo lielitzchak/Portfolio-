63e5e2ae3e683fa4a7092881a6fab8bc7d8feebd
Liel Itzchak <liel.izcha321@gmail.com>
2021-12-18 22:35:32 +0200

    j

---
+++ Component/Footer.jsx
@@ -0,0 +1,38 @@
+
+
+function Footer() {
+  return (
+    <footer>
+      <section id="sectionFooter">
+        <a href="https://github.com/lielitzchak">
+          <img
+            className="imgFooterContact"
+            src="./media/photos/github logo.png"
+            alt="Liel Itzchak github"
+          />
+        </a>
+        <a href="mailto:liel.izcha321@gmail.com">
+          <img
+            className="imgFooterContact"
+            src="./media/photos/gmail logo.png"
+            alt="Liel Itzchak email"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/lielitzchak/">
+          <img
+            className="imgFooterContact"
+            src="./media/photos/linkedin logo.png"
+            alt="Liel Itzchak linkedin"
+          />
+        </a>
+        <a href="https://did.li/Qtarl">
+          <img
+            className="imgFooterContact"
+            src="./media/photos/whatsapp logo.png"
+            alt="Liel Itzchak whatsapp"
+          />
+        </a>
+      </section>
+    </footer>
+  );
+}
